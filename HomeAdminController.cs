e1f315e43d63df81392d8434a53336da58b82e85
Hoàng Kim Quang <99950306+KimQuang19@users.noreply.github.com>
2024-12-30 15:06:28 +0700

    Add files via upload

---
+++ HomeAdminController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebVali.Models;
+using X.PagedList;
+
+namespace WebVali.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        QlbanValiContext db=new QlbanValiContext(); 
+        [Route("")]
+        [Route("index")]
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("danhmucsanpham")]
+        public IActionResult DanhMucSanPham(int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstsanpham, pageNumber, pageSize);
+            return View(lst);
+        }
+        [Route("ThemSanPhamMoi")]
+        [HttpGet]
+        public IActionResult ThemSanPhamMoi()
+        {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(),"MaChatLieu","ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList(), "MaDt", "TenLoai");
+            return View();
+        }
+        [Route("ThemSanPhamMoi")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemSanPhamMoi(TDanhMucSp sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TDanhMucSps.Add(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("DanhMucSanPham");
+            }
+            return View(sanPham);
+        }
+        [Route("SuaSanPham")]
+        [HttpGet]
+        public IActionResult SuaSanPham(string maSanPham)
+        {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList(), "MaDt", "TenLoai");
+            var sanPham = db.TDanhMucSps.Find(maSanPham);
+            return View(sanPham);
+        }
+        [Route("SuaSanPham")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SuaSanPham(TDanhMucSp sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("DanhMucSanPham","HomeAdmin");
+            }
+            return View(sanPham);
+        }
+        [Route("XoaSanPham")]
+        [HttpGet]
+        public IActionResult XoaSanPham(string maSanPham)
+        {
+            TempData["Message"] = "";
+
+            // Kiểm tra sản phẩm có trong danh mục
+            var chiTietSanPhams = db.TChiTietSanPhams.Where(x => x.MaSp == maSanPham).ToList();
+            if (chiTietSanPhams.Any())
+            {
+                TempData["Message"] = "Không xóa được sản phẩm này vì có liên kết";
+                return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+            }
+
+            // Xóa ảnh sản phẩm nếu có
+            var anhSanPham = db.TAnhSps.Where(x => x.MaSp == maSanPham);
+            if (anhSanPham.Any())
+            {
+                db.RemoveRange(anhSanPham);
+            }
+
+            // Xóa sản phẩm
+            var sanPham = db.TDanhMucSps.FirstOrDefault(x => x.MaSp == maSanPham);
+            if (sanPham != null)
+            {
+                db.Remove(sanPham);
+                db.SaveChanges();
+                TempData["Message"] = "Sản phẩm đã được xóa";
+            }
+            else
+            {
+                TempData["Message"] = "Không tìm thấy sản phẩm để xóa";
+            }
+
+            return RedirectToAction("DanhMucSanPham", "HomeAdmin");
+        }
+
+    }
+}
